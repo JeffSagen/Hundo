ece2c4dca0c014a5420110f98158b559869db946
JSagen <kasrkin76@gmail.com>
2021-11-26 03:07:13 -0800

    changed app.html

--- js/site.js
+++ js/site.js
@@ -1,6 +1,30 @@
-//call Hello
-function helloWorld(){
-    alert("Hello World!");
+//enter values
+//get the values form the Page
+//starts or controller function
+
+function getValues(){
+    //get values from the page
+let startValue = document.getElementById("startValue")
+    //we call generateNumbers
+
+    // we call displayNumbers
 }
 
-helloWorld()
+
+
+//generate numbers from startValue to the EndValue
+//logic function
+function generateNumbers(){
+
+}
+
+
+
+//display the numbers and mark even numbers bold
+//display of view functions
+function displayNumbers(){
+
+}
+
+//click on button
+
