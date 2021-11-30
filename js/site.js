12bbba3dfd6910201a130d898d95b7ae8bd15b76
JSagen <kasrkin76@gmail.com>
2021-11-30 01:47:23 -0800

    After Loop project

--- js/site.js
+++ js/site.js
@@ -4,25 +4,70 @@
 
 function getValues(){
     //get values from the page
-let startValue = document.getElementById("startValue")
-    //we call generateNumbers
+let startValue = document.getElementById("startValue").value;
+let endValue = document.getElementById("endValue").value;
 
-    // we call displayNumbers
+
+//Weneed to validate our input
+//parse into Integers
+startValue = parseInt(startValue);
+endValue = parseInt(endValue);
+
+//boolean check
+if (Number.isInteger(startValue) && Number.isInteger(endValue)) {
+        //we call generateNumbers
+let numbers = generateNumbers(startValue, endValue);
+ // we call displayNumbers
+ displayNumbers(numbers);
+}else{
+    alert("You Must enter integers")
+}
+
+    function newFunction() {
+        return document.getElementById("endValue").value;
+    }
+   
+    
 }
 
 
 
 //generate numbers from startValue to the EndValue
 //logic function
-function generateNumbers(){
-
+function generateNumbers(startValue, endValue){
+let numbers = [];
+//we want to get all numbers from start to end
+for (let index = startValue; index <= endValue; index++){
+// this will execute in a loop until index = endValue
+numbers.push(index);
+    }
+  return numbers;  
 }
 
-
-
-//display the numbers and mark even numbers bold
+    
 //display of view functions
-function displayNumbers(){
+
+function displayNumbers(numbers){
+
+    let templateRows = "";
+
+    for (let index = 0; index < numbers.length; index++) {
+        
+        let className = "even";
+        
+        let number = numbers[index];
+
+        //equality check
+        if(number % 2 == 0){
+            className = "even";
+        }else{
+            className = "odd"
+        }
+
+        templateRows += `<tr><td class="${className}">${number}</td></tr>`
+        
+    }
+    document.getElementById("results").innerHTML = templateRows
 
 }
 
